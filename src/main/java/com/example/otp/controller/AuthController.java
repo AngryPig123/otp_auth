4a3ef58d5a377dae72e0db6e4b75e1d2eca24916
Angrypig123 <shguddnr3@naver.com>
2024-02-27 21:23:22 +0900

    otp check response data type update

--- src/main/java/com/example/otp/controller/AuthController.java
+++ src/main/java/com/example/otp/controller/AuthController.java
@@ -3,7 +3,10 @@ package com.example.otp.controller;
 import com.example.otp.entity.Otp;
 import com.example.otp.entity.User;
 import com.example.otp.service.UserService;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,9 +28,10 @@ public class AuthController {
     }
 
     @PostMapping(path = "/user/auth")
-    public ResponseEntity<String> auth(@RequestBody User user) {
+    public ResponseEntity<RequestBodyContainer<String>> auth(@RequestBody User user) {
         String otpCode = userService.auth(user);
-        return new ResponseEntity<>(otpCode, HttpStatus.OK);
+        log.info("otp code = {}", otpCode);
+        return new ResponseEntity<>(new RequestBodyContainer<>(otpCode), HttpStatus.OK);
     }   //  ToDO otp code 를 response 한다.
 
     @PostMapping(path = "/otp/check")
@@ -39,6 +43,17 @@ public class AuthController {
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
 
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class RequestBodyContainer<T> {
+        private T requestBodyData;
+
+        public RequestBodyContainer(T requestBodyData) {
+            this.requestBodyData = requestBodyData;
+        }
     }
 
 }
--- src/test/java/com/example/otp/controller/ControllerTest.java
+++ src/test/java/com/example/otp/controller/ControllerTest.java
@@ -20,6 +20,7 @@ import java.util.Optional;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
